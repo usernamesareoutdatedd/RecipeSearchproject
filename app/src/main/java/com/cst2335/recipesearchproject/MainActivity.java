0ae989cd84c579b6adee7f8ded80ba54ac6b50d1
Campbell Harrison <harr0666@algonquinlive.com>
2022-04-03 11:26:41 -0400

    for now

--- app/src/main/java/com/cst2335/recipesearchproject/MainActivity.java
+++ app/src/main/java/com/cst2335/recipesearchproject/MainActivity.java
@@ -63,10 +63,46 @@ TextView tv2;
             public boolean onQueryTextChange(String s) {
                 adapter.getFilter().filter(s);
              if( s.equals ("Apple Pie")){
-                tv.setText("Testing!!");
+                tv.setText("APPLE PIE:");
+                tv2.setText("2 1/2 cups of flour" +
+                        "4 teaspoons  sugar" +
+                        "14 tablespoons of butter" +
+                        "1 large egg" +
+                        "3 pounds of apples" +
+                        "1/4 teaspoon of cinnamon" +
+                        "pinch of ground nutmeg");
                 }
              if( s.equals("Custard")){
-                 tv.setText("Yet another test");
+                 tv.setText("CUSTARD:");
+                 tv2.setText("4 cups of milk" +
+                         "1 tablespoon vanilla extract" +
+                         "1 teaspoon of butter" +
+                         "4 eggs" +
+                         "1/2 cup of sugar" +
+                         "3 tablespoons of cornstarch");
+             }
+
+             if(s.equals("Cookies")){
+                 tv.setText("COOKIES:");
+                 tv2.setText("2 1/4 cups of flour" +
+                         "1 teaspoon baking soda" +
+                         "1/2 teaspoon salt" +
+                         "1 cup butter" +
+                         "3/4 cups brown sugar" +
+                         "1 egg" +
+                         "2 cups of chocolate chips");
+             }
+
+             if(s.equals("Brownies")){
+                 tv.setText("BROWNIES:");
+                 tv2.setText("1/2 cup butter" +
+                         "1 cup white sugar" +
+                         "2 eggs" +
+                         "1 teaspoon vanilla extract" +
+                         "1/2 cup of flour" +
+                         "1/4 teaspoon of salt" +
+                         "1/3 cup cocoa powder" +
+                         "1/4 teaspoon baking powder");
              }
                 return false;
             }
